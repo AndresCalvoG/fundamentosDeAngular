80c139c521d92bb5770355aabb2677f9f5848306
AndresCalvoG <andrescalvo9407@gmail.com>
2022-09-15 22:00:57 -0500

    end class 7 property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,10 @@ export class AppComponent {
   age: number = 28;
   img =
     'https://static.remove.bg/remove-bg-web/c4b29bf4b97131238fda6316e24c9b3606c18000/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg';
+  btnDisabled = true;
+  car = {
+    model: 2014,
+    motor: 1200,
+    marca: 'chevrolet',
+  };
 }
