4ea3c9645ff30ffb1f5311d622cdf996e5d989bd
AndresCalvoG <andrescalvo9407@gmail.com>
2022-09-15 21:43:59 -0500

    end class 6 string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  //las variables deben ser publicas para usarlas en el template
+  name: string = 'andres';
+  age: number = 28;
+  img =
+    'https://static.remove.bg/remove-bg-web/c4b29bf4b97131238fda6316e24c9b3606c18000/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg';
 }
