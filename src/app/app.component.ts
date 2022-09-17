2a446078fb047b55381c5e2744059c3e4960efc7
AndresCalvoG <andrescalvo9407@gmail.com>
2022-09-17 06:32:38 -0500

    end class 14 [ngSwitch]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
     'https://static.remove.bg/remove-bg-web/c4b29bf4b97131238fda6316e24c9b3606c18000/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg';
   btnDisabled = true;
   car = {
-    model: 2014,
+    model: 2016,
     motor: 1200,
     marca: 'chevrolet',
   };
