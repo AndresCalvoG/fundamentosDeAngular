8cfb98f53003ef28128b34dc6c944a623363bd36
AndresCalvoG <andrescalvo9407@gmail.com>
2022-09-20 21:02:13 -0500

    end class 11 comunication father - children

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -7,34 +7,41 @@ import { Product } from '../../models/product.model';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  myShoppingCart: Product[] = [];
+  total: number = 0;
   products: Product[] = [
     {
       id: '1',
       name: 'EL mejor juguete',
       price: 565,
-      image: './assets/images/toy.jpg'
+      image: './assets/images/toy.jpg',
     },
     {
       id: '2',
       name: 'Bicicleta casi nueva',
       price: 356,
-      image: './assets/images/bike.jpg'
+      image: './assets/images/bike.jpg',
     },
     {
       id: '3',
       name: 'Colleción de albumnes',
       price: 34,
-      image: './assets/images/album.jpg'
+      image: './assets/images/album.jpg',
     },
     {
       id: '4',
       name: 'Mis libros',
       price: 23,
-      image: './assets/images/books.jpg'
+      image: './assets/images/books.jpg',
     },
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddToShoppingCart(product: Product) {
+    this.myShoppingCart.push(product);
+    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+  }
 }
